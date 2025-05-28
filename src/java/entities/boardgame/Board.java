3c1201beb3e1ece55d77d428f4b41c2dadb068c4
JAndersonArruda <ferrazarrudaanderson@gmail.com>
2025-05-28 15:18:50 -0300

    feat: chess layer and printing the board

--- src/java/entities/boardgame/Board.java
+++ src/java/entities/boardgame/Board.java
@@ -1,5 +1,7 @@
 package entities.boardgame;
 
+import entities.chess.ChessPiece;
+
 public class Board {
     private Integer rows;
     private Integer columns;
@@ -26,4 +28,14 @@ public class Board {
     public void setColumns(Integer columns) {
         this.columns = columns;
     }
+
+    public Piece piece(int row, int column) {
+        return pieces[row][column];
+    }
+
+    public Piece piece(Position position) {
+        return pieces[position.getRow()][position.getColumn()];
+    }
+
+
 }
---
+++ src/java/entities/chess/ChessPiece.java
@@ -0,0 +1,17 @@
+package entities.chess;
+
+import entities.boardgame.Board;
+import entities.boardgame.Piece;
+
+public class ChessPiece extends Piece {
+    private Color color;
+
+    public ChessPiece(Board board, Color color) {
+        super(board);
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+}
